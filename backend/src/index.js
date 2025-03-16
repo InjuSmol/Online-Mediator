8734a4604a2f6d585ab5f6bceefb3baf1bb2964d
VariA sMOL <96552183+VariaSmol@users.noreply.github.com>
2025-03-16 19:50:23 -0400

    finished messages backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser"
 
 import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js";
+
 
 dotenv.config()
 const app = express();
@@ -12,7 +14,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // to extract json out of body
 app.use(cookieParser());
-app.use("/api/auth", authRoutes)
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 
 app.listen(PORT, () => {
     console.log("server is runniing on PORT: " + PORT);
