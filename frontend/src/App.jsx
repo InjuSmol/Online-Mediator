18ba49bfd6a9571312f9179680f758511052b4d0
VariA sMOL <96552183+VariaSmol@users.noreply.github.com>
2025-04-11 01:33:54 -0400

    2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { useEffect } from "react"
 import { useAuthStore } from "./store/useAuthStore"
 //import { axiosInstance } from "./lib/axios"
 import  { Loader } from "lucide-react"
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
 
@@ -39,7 +40,7 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
-
+      <Toaster />
     </div>
   )
 }
