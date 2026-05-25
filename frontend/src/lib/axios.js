a9744d0bfd429eb5d4cc820693bb1a0040dc8e7e
V Smol <96552183+InjuSmol@users.noreply.github.com>
2026-05-24 23:41:45 -0400

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "http://18.188.75.118:5001",//"http://localhost:5001/api",
     withCredentials: true // send the cookies in every request
 })
