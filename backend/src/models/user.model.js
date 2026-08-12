0b2c295adba981e5856f0b9641cda9736879341a
V Smol <96552183+InjuSmol@users.noreply.github.com>
2026-08-11 23:16:57 -0400

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,30 +3,50 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     phone: {
-    type: String,
-    required: true,
-    unique: true,
-    }, 
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    phoneVerified: {
+      type: Boolean,
+      default: false,
+    },
+
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
     },
+
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
     fullName: {
       type: String,
       required: true,
+      trim: true,
     },
+
     password: {
       type: String,
       required: true,
       minlength: 6,
     },
+
     profilePic: {
       type: String,
       default: "",
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 const User = mongoose.model("User", userSchema);
