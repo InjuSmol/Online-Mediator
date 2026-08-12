b5f7215ccae1571984d6b27dfe5cec860186d226
V Smol <96552183+InjuSmol@users.noreply.github.com>
2026-08-11 23:21:37 -0400

    Add conversationId field to message schema

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -2,25 +2,44 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+      index: true,
+    },
+
     senderId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
+
     receiverId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
+
     text: {
       type: String,
+      default: "",
     },
+
     image: {
       type: String,
+      default: "",
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
+
+messageSchema.index({
+  conversationId: 1,
+  createdAt: 1,
+});
 
 const Message = mongoose.model("Message", messageSchema);
 
